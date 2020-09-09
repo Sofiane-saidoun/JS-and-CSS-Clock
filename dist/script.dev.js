a0483086f3c44b4b79b28aea1bfae1416d7d3a18
Sofiane <sofiane.saidoun@gmail.com>
2020-09-09 04:18:15 +0200

    *

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function setDate() {
+  var secondHand = document.querySelector(".second-hand");
+  var now = new Date();
+  var seconds = now.getSeconds();
+  var secondsDegrees = seconds / 60 * 360;
+  secondHand.style.transform = "rotate(".concat(secondsDegrees, "deg)");
+}
+
+setInterval(setDate, 1000);
---
+++ script.js
@@ -0,0 +1,9 @@
+function setDate() {
+  const secondHand = document.querySelector(".second-hand");
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
