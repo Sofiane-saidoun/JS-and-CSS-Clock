752560b1113b331482417a0a3798324ce951c500
Sofiane <sofiane.saidoun@gmail.com>
2020-09-09 20:48:13 +0200

    without transition

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -2,10 +2,21 @@
 
 function setDate() {
   var secondHand = document.querySelector(".second-hand");
+  var minuteHand = document.querySelector(".minute-hand");
+  var hourHand = document.querySelector(".hour-hand");
   var now = new Date();
   var seconds = now.getSeconds();
-  var secondsDegrees = seconds / 60 * 360;
+  var secondsDegrees = seconds / 60 * 360 + 90;
   secondHand.style.transform = "rotate(".concat(secondsDegrees, "deg)");
+  console.log("secondes :".concat(seconds));
+  var minutes = now.getMinutes();
+  var minutesDegrees = minutes / 60 * 360 + 90;
+  minuteHand.style.transform = "rotate(".concat(minutesDegrees, "deg)");
+  console.log("minutes :".concat(minutes));
+  var hours = now.getHours();
+  var hoursDegrees = hours / 12 * 360 + 90;
+  hourHand.style.transform = "rotate(".concat(hoursDegrees, "deg)");
+  console.log("heures :".concat(hours));
 }
 
 setInterval(setDate, 1000);
--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function setDate() {
   const secondHand = document.querySelector(".second-hand");
+  const minuteHand = document.querySelector(".minute-hand");
+  const hourHand = document.querySelector(".hour-hand");
+
   const now = new Date();
   const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360;
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  console.log(`secondes :${seconds}`);
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  console.log(`minutes :${minutes}`);
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  console.log(`heures :${hours}`);
 }
 
 setInterval(setDate, 1000);
